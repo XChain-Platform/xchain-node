a0e89ed6ae55d9f0269f59ab0e34ca44b64d6f6c
pataegrillo <javier@contimita.com>
2026-03-02 18:53:28 +0100

    - Added ulimit when running any crypto node with docker

--- src/services/NodeService.js
+++ src/services/NodeService.js
@@ -94,6 +94,7 @@ async function buildCryptoNode(coin, network, bitcoinVer = null) {
             const dockerCommand = 'docker run -d '
                 + '-v ' + dataDir + "/" + NODE_MODULE_NAME + "/" + coin + "/" + network + ":/root/." + coin + " "
                 + '--hostname ' + NODE_MODULE_NAME + ' '
+                + '--ulimit nofile=2048:2048 '
                 + '--network ' + getDockerNetwork(coin, network) + ' '
                 + (defaultExposedPort && defaultNodePort ? '-p ' + defaultExposedPort + ':' + defaultNodePort + ' ' : "")
                 + ' -e CRYPTO_NODE_VERSION=' + bitcoinVer + ' '
