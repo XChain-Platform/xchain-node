434d2af53f222d46039d13241398b97c82ee9bdd
J-Dog <j-dog@j-dog.net>
2026-07-16 14:53:54 -0700

    chore(node): license header sweep

--- scripts/run-multichain-e2e.sh
+++ scripts/run-multichain-e2e.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+#*********************************************************************
+#
+# Copyright © 2025-2026 Dankest, LLC
+# Based on XChain Platform by Dankest, LLC - https://dankest.llc
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This file is part of XChain Platform. Licensed under the GNU Affero
+# General Public License v3.0 or later; see LICENSE.md. A commercial
+# license (without AGPL source-disclosure terms) is available -
+# contact legal@dankest.llc.
+#
+#*********************************************************************
+
 #
 # run-multichain-e2e.sh - run the xchain-e2e-test full suite across every chain.
 #
